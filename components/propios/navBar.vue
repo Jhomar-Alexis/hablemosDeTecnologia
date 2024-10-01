64d96c1330493881bce870adf60833c43fb24f52
Jhomar-Alexis <113156364+Jhomar-Alexis@users.noreply.github.com>
2024-10-01 17:51:33 -0500

    tic

--- components/propios/navBar.vue
+++ components/propios/navBar.vue
@@ -5,7 +5,7 @@
         <!-- logo -->
         <div class="flex items-center">
             <img src="~/public/images/portada.jpeg" alt="logo" class="w-8 h-8 sm:w-10 sm:h-10 rounded-full">
-            <h1 class="text-xl font-bold ml-2">TICS</h1>
+            <h1 class="text-xl font-[900] ml-2">TIC</h1>
         </div>
         <div class="flex items-center gap-1">
             <!-- regresar -->
@@ -28,7 +28,8 @@
     <div ref="sidebar"
         class="hidden sm:flex lg:items-center sm:-mt-[3.2rem] pt-2 sm:pt-0 sm:pl-6 md:pl-0 justify-center">
         <div class="grid sm:flex gap-4 lg:gap-8 border sm:border-none font-bold bg-gray-200
-            sm:p-0 shadow-[0_0_20px_0] sm:shadow-none p-20 rounded-3xl text-center text-black" style="cursor: pointer;">
+            sm:p-0 shadow-[0_0_20px_0] sm:shadow-none p-20 rounded-3xl text-center text-black"
+            style="cursor: pointer;">
             <a v-for="link in links" :key="link" @click.prevent="scrollTo(link.target)">
                 <Icon :name="link.icon" class=" text-2xl sm:text-xl" />
                 <br /> {{ link.name }}
