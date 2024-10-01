7872e5f4dcb275db8da0438e0471bf161efc7d4f
Jhomar-Alexis <113156364+Jhomar-Alexis@users.noreply.github.com>
2024-10-01 17:21:30 -0500

    version final

--- components/propios/usos.vue
+++ components/propios/usos.vue
@@ -4,13 +4,13 @@
             Usos de la Tecnología
         </h1>
         <div class="flex flex-wrap justify-center">
-            <div class="w-60 lg:w-80 mb-4 bg-white p-4 rounded-lg shadow-[0_0_12px_0] shadow-black mx-4">
+            <div class="w-60 lg:w-80 mb-4 bg-cyan-700 p-4 rounded-lg shadow-[0_0_12px_0] shadow-black mx-4">
                 <div class="pb-4">
                     <img src="~/public/images/7.jpeg" alt="Tecnología"
-                        class="w-full object-cover border-4 border-cyan-700 rounded-lg" />
+                        class="w-full object-cover border-4 border-white rounded-lg" />
                 </div>
-                <div class="bg-cyan-700 p-2 rounded-lg">
-                    <p class="text-white font-semibold text-center">
+                <div class="bg-white p-2 rounded-lg">
+                    <p class="text-black font-semibold text-center">
                         Permite el acceso rápido a la información.
                     </p>
                 </div>
@@ -26,13 +26,13 @@
                     </p>
                 </div>
             </div>
-            <div class="w-60 lg:w-80 mb-4 bg-white p-4 rounded-lg shadow-[0_0_12px_0] shadow-black mx-4">
+            <div class="w-60 lg:w-80 mb-4 bg-cyan-700 p-4 rounded-lg shadow-[0_0_12px_0] shadow-black mx-4">
                 <div class="pb-4">
                     <img src="~/public/images/6.jpeg" alt="Tecnología"
-                        class="w-full object-cover border-4 border-cyan-700 rounded-lg" />
+                        class="w-full object-cover border-4 border-white rounded-lg" />
                 </div>
-                <div class="bg-cyan-700 p-2 rounded-lg">
-                    <p class="text-white font-semibold text-center">
+                <div class="bg-white p-2 rounded-lg">
+                    <p class="text-black font-semibold text-center">
                         Ayuda a realizar tareas virtuales escolares.
                     </p>
                 </div>
